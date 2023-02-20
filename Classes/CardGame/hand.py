21459fa7aa5517508b18500e1405f2f0dc47ff43
FuRong1213-vibenshus <rf@nextkbh.dk>
2023-02-20 09:59:05 +0100

    Hand modified

--- Classes/CardGame/hand.py
+++ Classes/CardGame/hand.py
@@ -12,19 +12,7 @@ class Hand(Pile):
         value = 0
         numberOfAces = 0
 
-        # Add the value for the non-ace cards:
-        for card in self.cards:
-            if card.rank == 'Ace':
-                numberOfAces += 1
-            else:
-                value += card.value  # Numbered cards are worth their number.
-
-        # Add the value for the aces:
-        value += numberOfAces  # Add 1 per ace.
-        for i in range(numberOfAces):
-            # If another 10 can be added without busting, do so:
-            if value + 10 <= 21:
-                value += 10
+        ## TBD
 
         return value
     
