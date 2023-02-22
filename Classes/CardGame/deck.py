d01b0d252fee8928cafc5c06e0bdf953744209c1
FuRong1213-vibenshus <rf@nextkbh.dk>
2023-02-22 09:58:21 +0100

    deck changed

--- Classes/CardGame/deck.py
+++ Classes/CardGame/deck.py
@@ -5,9 +5,9 @@ from pile import *
 class Deck(Pile):
     def __init__(self) -> None:
         self.cards = [
-            Card(rank, suit, value)
+            Card(rank, suit, RANKS[rank])
             for rank in RANKS
             for suit in SUITS
-            for value in RANKS.values()
+            #for value in RANKS.values()
         ]
         self.shuffle()
